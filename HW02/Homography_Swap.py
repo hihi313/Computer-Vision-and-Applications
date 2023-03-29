6bc89e6d50045cde5a84b16dc521c9ed2b9210d3
user <M10902117@mail.ntust.edu.tw>
2023-03-29 16:19:23 +0800

    finished HW02

--- HW02/Homography_Swap.py
+++ HW02/Homography_Swap.py
@@ -18,50 +18,80 @@ def imshow(name: str, img: np.ndarray, save: bool = False, ext: str = "jpg"):
     if img.shape[-1] == 1 or img.ndim == 2:
         plt.imshow(img, cmap='gray')
         plt.colorbar()
+        if save:
+            plt.imsave(f"{str(OUTPUT_DIR)}/{name}.{ext}", img)
     else:
-        plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-    if save:
-        plt.imsave(f"{str(OUTPUT_DIR)}/{name}.{ext}", img)
+        tmp = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        plt.imshow(tmp)
+        if save:
+            plt.imsave(f"{str(OUTPUT_DIR)}/{name}.{ext}", tmp)
     fig.show()
 
 
+def get_poly_mask(ref_img: np.ndarray, points: np.ndarray, inv: bool = False, dtype: np.dtype = np.uint8, line_type: int = cv.LINE_AA):
+    mask = np.zeros_like(ref_img, dtype=dtype)
+    cv.fillPoly(mask, [points.reshape((-1, 1, 2))],
+                (255, 255, 255), lineType=line_type)
+    imshow("mask", mask)
+    if inv:
+        inv_mask = cv.bitwise_not(mask)
+        imshow("inv mask", inv_mask)
+        return mask, inv_mask
+    return mask, None
+
+
 # %%
-img = cv.imread("ArtGallery.jpg", cv.IMREAD_GRAYSCALE)
-# img1 = np.zeros_like(img)
-# img2 = np.zeros_like(img)
-
-# img1[:, :1140] = img[:, :1140]
-# img2[:, 1140:] = img[:, 1140:]
-
-# imshow("img1", img1)
-# imshow("img2", img2)
+img = cv.imread("ArtGallery.jpg")
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+N_row, N_col, N_channel = img.shape
 
 # %%
-p = [(256, 211),
-     (677, 309),
-     (248, 945),
-     (676, 874)]
-p = np.array(p)
-p_ = [(1378, 401),
+pL = [(256, 211),
+      (677, 309),
+      (676, 874),
+      (248, 945)]
+pL = np.array(pL)
+
+pR = [(1378, 401),
       (1676, 326),
-      (1381, 775),
-      (1683, 806)]
-p_ = np.array(p_)
+      (1683, 806),
+      (1381, 775)]
+pR = np.array(pR)
 
-N = min(len(p), len(p_)) # #points
-# %%
+# points
+N = min(len(pL), len(pR))
 
-A = np.zeros((N*2, 9))
-for i in range(0, N):
-    A[i*2, :] = [0, 0, 0,
-               -p[i, 0], -p[i, 1], -1,
-               p_[i, 1]*p[i, 0], p_[i, 1]*p[i, 1], p_[i, 1]]
-    A[i*2+1, :] = [p[i, 0], p[i, 1], 1,
-               0, 0, 0,
-               -p_[i, 0]*p[i, 0], -p_[i, 0]*p[i, 1], -p_[i, 0]]
+maskR, inv_maskR = get_poly_mask(img_gray, pR, inv=True)
+maskL, inv_maskL = get_poly_mask(img_gray, pL, inv=True)
 
 # %%
-U, S, V = LA.svd(A)
-H = V[:, -1].reshape((3, 3))
+
+# Find homography
+H, mask = cv.findHomography(pL, pR, 0)  # Use least-square
+
+# Left to right
+frameR = cv.warpPerspective(img, H, (N_col, N_row),
+                            flags=cv.INTER_CUBIC,
+                            borderMode=cv.BORDER_CONSTANT)
+frameR = cv.bitwise_and(frameR, frameR, mask=maskR)
+imshow("frameR", frameR, save=True)
+
+# Right to left
+frameL = cv.warpPerspective(img, LA.inv(H), (N_col, N_row),
+                            flags=cv.INTER_CUBIC,
+                            borderMode=cv.BORDER_CONSTANT)
+frameL = cv.bitwise_and(frameL, frameL, mask=maskL)
+imshow("frameL", frameL, save=True)
 # %%
 
+# Fill left
+img_bgL = cv.bitwise_and(img, img, mask=inv_maskL)
+resultL = cv.add(img_bgL, frameL)
+imshow("resultL", resultL)
+
+# Fill right
+img_bgR = cv.bitwise_and(resultL, resultL, mask=inv_maskR)
+result = cv.add(img_bgR, frameR)
+imshow("M10902117", result, save=True)
+
+# %%

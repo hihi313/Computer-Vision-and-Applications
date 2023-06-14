b3c700e9ac21eb347ce4ab951988863833c95c25
user <M10902117@mail.ntust.edu.tw>
2023-06-14 18:16:22 +0800

    added tqdm, testing whole dataset images

--- Final/reconstruct.py
+++ Final/reconstruct.py
@@ -1,13 +1,13 @@
 # %% Import
 import copy
 import imghdr
-import math
 import os
 from pathlib import Path
 from typing import List, Tuple
 import cv2 as cv
 from matplotlib import pyplot as plt
 import numpy as np
+from tqdm.auto import tqdm
 
 # %% Global vars
 ROOT = Path("/app/Final")  # Path(os.path.dirname(os.path.abspath('__file__')))
@@ -143,23 +143,23 @@ def epilines(F: np.ndarray, points: np.ndarray, is_left: bool,
     return lines.squeeze()
 
 
-images = load_images(IMG_DIR)
-image_pairs = split_images(images)
-# A test image pair
-idx = 33
-L_img = image_pairs[idx][0]
-R_img = image_pairs[idx][1]
-imshow("L_img", L_img)
-imshow("L_img", R_img)
-# 1. Brightest pixel each row
-# Get top N brightest point as match candidate?
-L_px = get_max_px_row(L_img)
-R_px = get_max_px_row(R_img)
-# Display to check
-imshow("L max points", draw_point(L_img, L_px, radius=1), True)
-imshow("R max points", draw_point(R_img, R_px, radius=1), True)
-# 2. Get R points near L epilines
-R_epilines = epilines(F, L_px, True)
+# images = load_images(IMG_DIR)
+# image_pairs = split_images(images)
+# # A test image pair
+# idx = 100
+# L_img = image_pairs[idx][0]
+# R_img = image_pairs[idx][1]
+# imshow("L_img", L_img)
+# imshow("L_img", R_img)
+# # 1. Brightest pixel each row
+# # Get top N brightest point as match candidate?
+# L_px = get_max_px_row(L_img)
+# R_px = get_max_px_row(R_img)
+# # Display to check
+# imshow("L max points", draw_point(L_img, L_px, radius=1), True)
+# imshow("R max points", draw_point(R_img, R_px, radius=1), True)
+# # 2. Get R points near L epilines
+# R_epilines = epilines(F, L_px, True)
 
 # %% Get patch score image
 
@@ -216,8 +216,8 @@ def match_score_img(points, img, img_another,
     return np.array(score_imgs)
 
 
-score_method = cv.TM_SQDIFF_NORMED
-L_pts_sImgs = match_score_img(L_px, L_img, R_img, score_method=score_method)
+# score_method = cv.TM_SQDIFF_NORMED
+# L_pts_sImgs = match_score_img(L_px, L_img, R_img, score_method=score_method)
 # %% Get max/min score along epiline on score image
 
 
@@ -240,8 +240,8 @@ def get_match_on_score_img(epilines, pts_scoreImgs, score_method):
     return np.array(matches)
 
 
-L_matches = get_match_on_score_img(
-    R_epilines, L_pts_sImgs, score_method=score_method)
+# L_matches = get_match_on_score_img(
+#     R_epilines, L_pts_sImgs, score_method=score_method)
 # %% Draw match
 
 
@@ -280,11 +280,11 @@ def draw_match(image_l: np.ndarray, point_l: np.ndarray,
     return cv.addWeighted(img_black, alpha, image, (1-alpha), 0)
 
 
-imshow("match", draw_match(L_img, L_px, R_img, L_matches), True)
+# imshow("match", draw_match(L_img, L_px, R_img, L_matches), True)
 # %% Correct/adjust match
 
 # Test correctMatches
-L_px_before, L_matches_before = np.copy(L_px), np.copy(L_matches)
+# L_px_before, L_matches_before = np.copy(L_px), np.copy(L_matches)
 
 # L_px, L_matches = cv.correctMatches(F, L_px_before.reshape(1, -1, 2), L_matches_before.reshape(1, -1, 2))
 # L_px = L_px.reshape(-1, 2)
@@ -293,8 +293,8 @@ L_px_before, L_matches_before = np.copy(L_px), np.copy(L_matches)
 # imshow("correct match", draw_match(L_img, L_px, R_img, L_matches), True)
 # %% Test correctMatches
 
-print(L_px != L_px_before)
-print(L_matches != L_matches_before)
+# print(L_px != L_px_before)
+# print(L_matches != L_matches_before)
 
 # %% Get 3D points by triangulation
 
@@ -331,10 +331,10 @@ def get_3D_points(P_l: np.ndarray, points_l: np.ndarray,
     return points_3d, mask, np.count_nonzero(mask == False)  # outliers
 
 
-P_l, R_l, t_l = projection_matrix(K_l, RT_l, False)
-P_r, R_r, t_r = projection_matrix(K_r, RT_r, False)
+# P_l, R_l, t_l = projection_matrix(K_l, RT_l, False)
+# P_r, R_r, t_r = projection_matrix(K_r, RT_r, False)
 
-points_3d, inlier_mask, num_outliers = get_3D_points(P_l, L_px, P_r, L_matches)
+# points_3d, inlier_mask, num_outliers = get_3D_points(P_l, L_px, P_r, L_matches)
 # %% Get 3d points DLT
 
 
@@ -364,14 +364,14 @@ def get_3D_points_DLT(P_l: np.ndarray, points_l: np.ndarray,
     return x[:, :-1] / x[:, -1].reshape(-1, 1)
 
 
-points_3d_dlt = get_3D_points_DLT(P_l, L_px, P_r, L_matches)
+# points_3d_dlt = get_3D_points_DLT(P_l, L_px, P_r, L_matches)
 
 
 # %%
-def outlier_3d(points_3d: np.ndarray,
-               K_l: np.ndarray, R_l: np.ndarray, t_l, points_l: np.ndarray,
-               K_r: np.ndarray, R_r: np.ndarray, t_r, points_r: np.ndarray,
-               mask=None, outlier_th: float = 2) -> np.ndarray:
+def inlier_3d(points_3d: np.ndarray,
+              K_l: np.ndarray, R_l: np.ndarray, t_l, points_l: np.ndarray,
+              K_r: np.ndarray, R_r: np.ndarray, t_r, points_r: np.ndarray,
+              mask=None, outlier_th: float = 2) -> np.ndarray:
     N = len(points_3d)
     assert N == len(points_l) and N == len(points_r)
 
@@ -407,220 +407,50 @@ def outlier_3d(points_3d: np.ndarray,
     return inli_mask
 
 
-inlier_mask_dlt = outlier_3d(points_3d_dlt,
-                                    K_l, R_l, t_l, L_px,
-                                    K_r, R_r, t_r, L_matches, None)
+# inlier_mask_dlt = inlier_3d(points_3d_dlt,
+#                              K_l, R_l, t_l, L_px,
+#                              K_r, R_r, t_r, L_matches, None)
 # %% Draw 3d outlier
 
-imshow("3d outlier", draw_match(L_img, L_px, R_img, L_matches, inlier_mask), True)
-imshow("3d outlier dlt", draw_match(L_img, L_px, R_img, L_matches, inlier_mask_dlt), True)
+# imshow("3d outlier", draw_match(L_img, L_px, R_img, L_matches, inlier_mask), True)
+# imshow("3d outlier dlt", draw_match(L_img, L_px,
+#        R_img, L_matches, inlier_mask_dlt), True)
 
 # %% Save to XYZ file
 
-np.savetxt(f"{OUTPUT_DIR / 'points_3d.xyz'}", points_3d[inlier_mask], delimiter=' ')
-np.savetxt(f"{OUTPUT_DIR / 'points_3d DLT.xyz'}", points_3d_dlt[inlier_mask_dlt], delimiter=' ')
-
-
-# %% Get points near epilines
-
-
-def get_points_near_epilines(epilines: np.ndarray, candidate_points: np.ndarray,
-                             distance_th: float = None, num_candidate: int = 3):
-    """
-        epilines: N*3
-        candidate_points: candidate pixel locations, N_c*2
-    """
-    N = len(epilines)
-    N_c = len(candidate_points)
-    assert N > 0 and N_c > 0
-
-    candi_pnts_homo = homogeneous(candidate_points)
-    # N*3 * 3*N_c = N*N_c, N_c = #candidate points
-    dist = abs(epilines @ candi_pnts_homo.T)
-
-    # N*N_C*2, element is (candidate_point, distance)
-    epi_candi = []
-    for i in range(N):
-        candis = []
-        for j in range(N_c):
-            candis.append((candidate_points[j], dist[i][j]))
-        epi_candi.append(candis)
-
-    # Sort by distance for each row
-    epi_close = [sorted(candis, key=lambda c: c[1])
-                 for candis in epi_candi]
-    # Filter by condition provided & remove distance info
-    if distance_th is not None:
-        # For each row, check elements' distance < th
-        epi_close = [[candi[0]
-                      for candi in closes if candi[1] < distance_th]
-                     for closes in epi_close]
-    elif num_candidate is not None:
-        # top N candidates/points close to epiline
-        epi_close = [[candi[0]
-                      for candi in closes[:num_candidate]]
-                     for closes in epi_close]
-    return epi_close
-
-
-def epiline_points(epilines, img_size):
-    h, w = img_size[:2]
-    line_pnts = []
-    for l in epilines:
-        a, b, c = l
-        pnts = []
-        for x in range(w):
-            y = -(c + a*x) / b
-            # half way round to even number
-            pnts.append((x, round(y)))
-        line_pnts.append(pnts)
-    return np.array(line_pnts)
-
-
-L_candis = epiline_points(R_epilines, R_img.shape[:2])
-# %% Get patch scores of (candidate) points
-
-
-def patch_scores(image_l: np.ndarray, points_l: np.ndarray,
-                 image_r: np.ndarray, candidates_r: np.ndarray,
-                 patch_size: int = 8, score_method: int = cv.TM_SQDIFF_NORMED):
-    """
-        For each left point, get patch scores of its right (candidate) points
-        points_l: N*2
-        candidates_r: N*?, ? is #candidates of the left point. 
-            (candidates on right image). May be empty
-    """
-    N, M, C = candidates_r.shape
-    assert N == len(points_l) and C == 2
-
-    # N*M*3 to store (x, y, score).Score init = 0
-    pts_scores_r = np.zeros((N, M, C+1))
-    pts_scores_r[:, :, :2] = candidates_r
-
-    # patchs' dict to reuse patches on right image
-    patDic_r = {}
-    for p_l, p_scores_r in zip(points_l, pts_scores_r):
-        patch_l = get_patch(image_l, p_l, patch_size)
-        for p_s_r in p_scores_r:
-            pr = tuple(p_s_r[:-1])  # convert to tuple key
-            patch_r = patDic_r[pr] if pr in patDic_r else get_patch(
-                image_r, pr, patch_size)
-            score = cv.matchTemplate(patch_l, patch_r, score_method)
-            p_s_r[-1] = score[0][0]
-    return pts_scores_r
-
-
-score_method = cv.TM_SQDIFF_NORMED
-L_candis_score = patch_scores(L_img, L_px,
-                              R_img, L_candis)
-# %% Get match by score
-
-# TODO: get first K match?
-
-
-def get_match(pts_score: np.ndarray,
-              score_method: int, score_th: float = None):
-    """
-        Sort &/ filter by score to get match
-    """
-    assert pts_score.shape[-1] == 3
-
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    take_min = True if score_method in [
-        cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED] else False
-
-    # Sort each row by score
-    idx = np.argsort(pts_score[:, :, 2], axis=1) if take_min else np.argsort(
-        pts_score[:, :, 2], axis=1)[::-1]
-    pts_scores_sort = np.take_along_axis(pts_score, idx[:, :, None], axis=1)
-    # Filter by score threshold
-    if score_th is not None:
-        mask = pts_scores_sort[:, :,
-                               2] < score_th if take_min else pts_scores_sort[:, :, 2] > score_th
-        pts_scores_sort = pts_scores_sort[mask]
-    return pts_scores_sort
-
-
-# N*M*3
-L_matches = get_match(L_candis_score, score_method)
-# %%
-L_matches = L_matches[:, 0, :2]
-
-
-def min_linear_eq(A: np.ndarray, b: np.ndarray = 0,
-                  method: str = "svd"):
-    method = method.casefold()
-    A = A.astype(np.float64)
-    x = None
-    is_homo = b is None or np.all(b == 0)
-    if method == "svd" and is_homo:
-        # Default is SVD
-        U, S, VH = np.linalg.svd(A)
-        V = VH.conj().T
-        x = V[:, -1]  # last column
-    return x
-# %% Get 3D point outlier by reprojection error
-
-
-def to_np(*args):
-    return [arg
-            if isinstance(arg, np.ndarray)
-            else np.array(arg)
-            for arg in args]
-
+# np.savetxt(f"{OUTPUT_DIR / 'points_3d.xyz'}",
+#            points_3d[inlier_mask], delimiter=' ')
+# np.savetxt(f"{OUTPUT_DIR / 'points_3d DLT.xyz'}",
+#            points_3d_dlt[inlier_mask_dlt], delimiter=' ')
 
 # %%
 if __name__ == "__main__":
     images = load_images(IMG_DIR)
     image_pairs = split_images(images)
-    # A test image pair
-    idx = 33
-    L_img = image_pairs[idx][0]
-    R_img = image_pairs[idx][1]
-    imshow("L_img", L_img)
-    imshow("L_img", R_img)
-    # 1. Brightest pixel each row
-    # Get top N brightest point as match candidate?
-    L_px = get_max_px_row(L_img)
-    R_px = get_max_px_row(R_img)
-    # Display to check
-    imshow("L max points", draw_point(L_img, L_px, radius=1), True)
-    imshow("R max points", draw_point(R_img, R_px, radius=1), True)
-    # 2. Get R points near L epilines
-    # TODO: use cv.correctMatches() to
-    # Get L epilines
-    L_epilines = epilines(F, L_px, True)
-    L_closes = get_points_near_epilines(L_epilines, R_px, num_candidate=10)
-    # 3. compute L, R points' patch score
-    score_method = cv.TM_SQDIFF_NORMED
-    # N*?*2, ? = #candidates, 2= (candidate point, score)
-    L_scores = patch_scores(
-        L_img, L_px, R_img, L_closes, score_method=score_method)
-    # 4. Get L, R matched points by score
-    L_matches = get_match(L_scores, score_method=score_method)
-    # Take 1st match's point only
-    L_matches = [m[0][0] if len(m) > 0 else m
-                 for m in L_matches]
-    print(len([m for m in L_matches if len(m) > 0]))
-    # cv::correctMatches to adjust
-    L_px = np.array(L_px).reshape(1, -1, 2)
-    L_matches = np.array(L_matches).reshape(1, -1, 2)
-    L_px, L_matches = cv.correctMatches(F, L_px, L_matches)
-    L_px = L_px.reshape(-1, 2)
-    L_matches = L_matches.reshape(-1, 2)
-
-    # Draw matched points to check
-    imshow("Matches", draw_match(L_img, L_px, R_img, L_matches), True)
-    # 5. Get 3D points
-    P_l, Rl_cw, tl_cw = projection_matrix(K_l, RT_l)
-    P_r, Rr_cw, tr_cw = projection_matrix(K_r, RT_r)
-    points_3d, mask = get_3D_points(P_l, L_px,
-                                    P_r, L_matches)
-
-    outlier_3d(points_3d, mask,
-               K_l, Rl_cw, tl_cw, L_px,
-               K_r, Rr_cw, tr_cw, L_matches)
-
-    np.savetxt(f"{OUTPUT_DIR / 'test.xyz'}", points_3d, delimiter=' ')
-    plt.show()
-# %%
+    result_3d = np.empty((0, 3))
+    for L_img, R_img in tqdm(image_pairs):
+        # A image pair
+        # 1. Brightest pixel each row
+        L_px = get_max_px_row(L_img)
+        # R_px = get_max_px_row(R_img)
+        # 2. Get R points near L epilines
+        R_epilines = epilines(F, L_px, True)
+        # 3. Get L points' score image
+        score_method = cv.TM_SQDIFF_NORMED
+        L_pts_sImgs = match_score_img(
+            L_px, L_img, R_img, score_method=score_method)
+        # 4. Search match point along epiline on score image
+        L_matches = get_match_on_score_img(
+            R_epilines, L_pts_sImgs, score_method=score_method)
+        # 5. triangulate
+        P_l, R_l, t_l = projection_matrix(K_l, RT_l, False)
+        P_r, R_r, t_r = projection_matrix(K_r, RT_r, False)
+        points_3d_dlt = get_3D_points_DLT(P_l, L_px, P_r, L_matches)
+        # 6. Get inlier if reprojection err < th
+        # inlier mask
+        inlier_mask_dlt = inlier_3d(points_3d_dlt,
+                                    K_l, R_l, t_l, L_px,
+                                    K_r, R_r, t_r, L_matches, None)
+        p3d_inline = points_3d_dlt[inlier_mask_dlt]
+        result_3d = np.append(result_3d, p3d_inline, axis=0)
+    np.savetxt(f"{OUTPUT_DIR / 'result_3d DLT.xyz'}", result_3d, delimiter=' ')
